3a575de42631dacc3cc31f6317b48590d0585cad
raunak <raunakmar2005@gmail.com>
2023-10-24 16:44:48 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,54 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Death Bed",
+    artist: "Powfu",
+    img: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+    src: "https://samplesongs.netlify.app/Death%20Bed.mp3",
+    
+  },
+  {
+    name: "Bad Liar",
+    artist: "Imagine Dragons",
+    img: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+    src: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+    
+  },
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+    src: "https://samplesongs.netlify.app/Faded.mp3",
+    
+  },
+  {
+    name: "Hate Me",
+    artist: "Ellie Goulding",
+    img: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+    src: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+    
+  },
+  {
+    name: "Solo",
+    artist: "Clean Bandit",
+    img: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+    src: "https://samplesongs.netlify.app/Solo.mp3",
+    
+  },
+  {
+    name: "Without Me",
+    artist: "Halsey",
+    img: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+    src: "https://samplesongs.netlify.app/Without%20Me.mp3",
+  }
+];
